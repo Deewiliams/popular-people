90aa9030e5736b4e7affa1f554b09fb85b3661f1
Desire <desireirankundawilliams@gmail.com>
2022-06-21 18:45:43 +0200

    designed the layout

---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Container,Row,Col,Card,Button } from 'react-bootstrap';
+import ListImages from './ListImages';
+
+const Header = () => {
+  return (
+    <>
+    <Container>
+  <Row >
+    <Col>
+    <Card bg="primary" >
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+    </Col>
+  </Row>
+  <br />
+  <ListImages />
+</Container>
+
+</>
+  )
+}
+
+export default Header
---
+++ src/Components/ListImages.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Container, Row, Col,Card,Button } from 'react-bootstrap';
+
+const ListImages = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm>
+                <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm><Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card></Col>
+                <Col sm><Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default ListImages
